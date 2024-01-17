95a2a33018ea9a92edf24e57b559832574f46850
AyDin VM <134310053+iam-aydin@users.noreply.github.com>
2024-01-17 22:06:04 +0330

    Update index.js
    route change from / to /markets

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import fs from 'fs';
 import axios from "axios";
-import { type } from "os";
 
 const app = express();
 const port = 3000;
@@ -11,7 +10,7 @@ const API_KEY_VALUE = fs.readFileSync('./api_key_value.txt', 'utf8').trim();
 
 
 // با درخواست زیر می‌توانید لیستی از بازارها و وضعیت آنها دریافت کنید
-app.get("/", async (req, res) => {
+app.get("/markets", async (req, res) => {
     const config = { 
         headers: {
             "X-API-Key": API_KEY_VALUE
