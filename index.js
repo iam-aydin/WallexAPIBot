0131caa88433081e65228fdfde7bee1948d41049
i-am-aydin <vm84aydinbusinessacc@gmail.com>
2024-01-17 22:52:54 +0330

    add views folder and an index.ejs file , write new route /marketsView in index.js to show the data on a table

--- index.js
+++ index.js
@@ -8,21 +8,37 @@ const API_URL = "https://api.wallex.ir";
 const KEY = "X-API-Key"
 const API_KEY_VALUE = fs.readFileSync('./api_key_value.txt', 'utf8').trim();
 
+app.use(express.static("public"));
 
 // با درخواست زیر می‌توانید لیستی از بازارها و وضعیت آنها دریافت کنید
 app.get("/markets", async (req, res) => {
-    const config = { 
-        headers: {
-            "X-API-Key": API_KEY_VALUE
-        }
-     }
-  try {
-    const response = await axios.get(`${API_URL}/v1/markets`, config);
-    res.send(response.data);
-  } catch (error) {
-    console.error(error);
-    res.status(401).json({ message: "Error 401 Unauthorized" });
+  const config = { 
+      headers: {
+          "X-API-Key": API_KEY_VALUE
+      }
    }
+try {
+  const response = await axios.get(`${API_URL}/v1/markets`, config);
+  res.send(response.data);
+} catch (error) {
+  console.error(error);
+  res.status(401).json({ message: "Error 401 Unauthorized" });
+ }
+});
+
+app.get("/marketsView", async (req, res) => {
+  const config = { 
+      headers: {
+          "X-API-Key": API_KEY_VALUE
+      }
+   }
+try {
+  const response = await axios.get(`${API_URL}/v1/markets`, config);
+  res.render('index.ejs', { result: response.data });
+} catch (error) {
+  console.error(error);
+  res.status(401).json({ message: "Error 401 Unauthorized" });
+ }
 });
 
 app.listen(port, () => {
