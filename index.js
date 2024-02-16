7c338cee71d4310c0e23ea5da203fe6e0deb9ac8
AyDin VM <134310053+iam-aydin@users.noreply.github.com>
2024-02-16 12:06:17 +0330

    Update index.js

--- index.js
+++ index.js
@@ -1,46 +1,31 @@
-import express from "express";
-import fs from 'fs';
-import axios from "axios";
+const express = require("express");
+require("dotenv").config();
+const axios = require("axios");
 
 const app = express();
 const port = 3000;
-const API_URL = "https://api.wallex.ir";
-const KEY = "X-API-Key"
-const API_KEY_VALUE = fs.readFileSync('./api_key_value.txt', 'utf8').trim();
+const API_URL = process.env.API_URL;
+const KEY = "X-API-Key";
+const API_KEY_VALUE = process.env.API_KEY_VALUE;
 
 app.use(express.static("public"));
 
-// با درخواست زیر می‌توانید لیستی از بازارها و وضعیت آنها دریافت کنید
+// You can get a list of markets and their status with the following request
 app.get("/markets", async (req, res) => {
-  const config = { 
-      headers: {
-          "X-API-Key": API_KEY_VALUE
-      }
-   }
-try {
-  const response = await axios.get(`${API_URL}/v1/markets`, config);
-  res.send(response.data);
-} catch (error) {
-  console.error(error);
-  res.status(401).json({ message: "Error 401 Unauthorized" });
- }
-});
-
-app.get("/marketsView", async (req, res) => {
-  const config = { 
-      headers: {
-          "X-API-Key": API_KEY_VALUE
-      }
-   }
-try {
-  const response = await axios.get(`${API_URL}/v1/markets`, config);
-  res.render('index.ejs', { result: response.data });
-} catch (error) {
-  console.error(error);
-  res.status(401).json({ message: "Error 401 Unauthorized" });
- }
+  const config = {
+    headers: {
+      "X-API-Key": API_KEY_VALUE,
+    },
+  };
+  try {
+    const response = await axios.get(`${API_URL}/v1/markets`, config);
+    res.send(response.data);
+  } catch (error) {
+    console.error(error);
+    res.status(401).json({ message: "Error 401 Unauthorized" });
+  }
 });
 
 app.listen(port, () => {
-    console.log(`Server is running on port : ${port}`)
-})
+  console.log(`Server is running on port : ${port}`);
+});
